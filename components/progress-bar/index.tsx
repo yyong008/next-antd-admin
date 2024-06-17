f46ea47aa6ea3f9be54de17ad5190da84c56ba8a
Magnesium- <yyong007@outlook.com>
2024-06-17 17:54:45 +0800

    feat: progress bar use antd token color primary

--- components/progress-bar/index.tsx
+++ components/progress-bar/index.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { AppProgressBar } from 'next-nprogress-bar';
+import { useAntdThemeToken } from '@/hooks';
 
 export function ProgressBar() {
+  const token = useAntdThemeToken();
   return (
     <AppProgressBar
       height="4px"
-      color="#1677ff"
+      color={token.colorPrimary}
       options={{ showSpinner: true }}
       shallowRouting
     />
